d2195503c38c65483a0ae14278f74d288fcbdaeb
Apoorva Machale <96104516+apoorvaa-m@users.noreply.github.com>
2023-12-10 08:55:40 -0800

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
-# client = MongoClient("mongodb+srv://sundrifafu:CPSC449@cluster0.co8msb3.mongodb.net/?retryWrites=true&w=majority")
-client = MongoClient("mongodb+srv://skmath1999:root@cluster0.7b28hxz.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://sundrifafu:CPSC449@cluster0.co8msb3.mongodb.net/?retryWrites=true&w=majority")
 
 db = client.cloud_service_db
 
